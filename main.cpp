3719e10c2a1ec769d1a51eddb1fd56e44113d7ee
zhang_zhuhao <zhangzh@jmed.com>
2024-09-03 18:51:17 +0800

    FIX: No paddings

--- main.cpp
+++ main.cpp
@@ -17,16 +17,19 @@
 #include <vtkSmartPointer.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
+    if (argc < 6) {
         std::cerr << "Usage: " << argv[0] << " Filename(.stl)"
-                  << " Dilate value"
+                  << " DilateValue(mm)"
                   << " Spacing(mm)"
-                  << " Padding(mm)" << std::endl;
+                  << " Padding(mm)"
+                  << " SimplificationRate(0.0-1.0)"
+                  << std::endl;
         return EXIT_FAILURE;
     }
     double dilate_value = atof(argv[2]);
     double spacing = atof(argv[3]);
     double padding = atof(argv[4]);
+    double simplification_rate = atof(argv[5]);
 
     vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
     reader->SetFileName(argv[1]);
@@ -34,7 +37,7 @@ int main(int argc, char *argv[]) {
 
     vtkSmartPointer<vtkDecimatePro> decimate = vtkSmartPointer<vtkDecimatePro>::New();
     decimate->SetInputData(reader->GetOutput());
-    decimate->SetTargetReduction(1.0);
+    decimate->SetTargetReduction(simplification_rate);
     decimate->PreserveTopologyOn();
     decimate->Update();
     vtkSmartPointer<vtkPolyData> poly_data = decimate->GetOutput();
@@ -60,52 +63,59 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < range_x; i++) {
         for (int j = 0; j < range_y; j++) {
             for (int k = 0; k < range_z; k++) {
-                std::array<double, 3> testPoint = {(i + 0.5) * sdf.m_spacing_x + aabb.m_x_min, (j + 0.5) * sdf.m_spacing_y + aabb.m_y_min, (k + 0.5) * sdf.m_spacing_z + aabb.m_z_min}; // Replace with your test point
+                double test_point[3] = {(i + 0.5) * sdf.m_spacing_x + aabb.m_x_min, (j + 0.5) * sdf.m_spacing_y + aabb.m_y_min, (k + 0.5) * sdf.m_spacing_z + aabb.m_z_min};
 
-                double closestPoint[3];
-                double closestPointDist2;
-                vtkIdType cellId;
-                int subId;
+                double origin[3] = {(i + 0.5) * sdf.m_spacing_x + aabb.m_x_min, aabb.m_y_max, (k + 0.5) * sdf.m_spacing_z + aabb.m_z_min};
+                double closest_point[3];
+                double closest_point_dist2;
+                vtkIdType cell_id;
+                int sub_id;
                 vtkSmartPointer<vtkGenericCell> cell = vtkSmartPointer<vtkGenericCell>::New();
+                cellLocator->FindClosestPoint(test_point, closest_point, cell, cell_id, sub_id, closest_point_dist2);
+                double distance = sqrt(closest_point_dist2);
 
-                cellLocator->FindClosestPoint(testPoint.data(), closestPoint, cell, cellId, subId, closestPointDist2);
+                // vtkSmartPointer<vtkIdList> cell_ids = vtkSmartPointer<vtkIdList>::New();
+                // cellLocator->FindCellsAlongLine(test_point, origin, 0.000001, cell_ids);
 
-                double distance = sqrt(closestPointDist2);
+                // if (distance <= spacing) {
+                //     distance = 0;
+                // }
+                // distance = (cell_ids->GetNumberOfIds() % 2 != 0) ? -distance : distance;
                 sdf.setValue(i, j, k, distance);
             }
         }
     }
 
-    vtkSmartPointer<vtkImageData> imageData = vtkSmartPointer<vtkImageData>::New();
-    imageData->SetDimensions(range_x, range_y, range_z);
-    imageData->AllocateScalars(VTK_DOUBLE, 1);
+    vtkSmartPointer<vtkImageData> image_data = vtkSmartPointer<vtkImageData>::New();
+    image_data->SetDimensions(range_x, range_y, range_z);
+    image_data->AllocateScalars(VTK_DOUBLE, 1);
 
     for (int x = 0; x < range_x; ++x) {
         for (int y = 0; y < range_y; ++y) {
             for (int z = 0; z < range_z; ++z) {
                 int index = x * range_y * range_z + y * range_z + z;
-                double *pixel = static_cast<double *>(imageData->GetScalarPointer(x, y, z));
+                double *pixel = static_cast<double *>(image_data->GetScalarPointer(x, y, z));
                 pixel[0] = sdf.m_volume_data[index];
             }
         }
     }
 
     vtkSmartPointer<vtkMarchingCubes> marchingCubes = vtkSmartPointer<vtkMarchingCubes>::New();
-    marchingCubes->SetInputData(imageData);
+    marchingCubes->SetInputData(image_data);
     marchingCubes->SetValue(0, dilate_value);
 
     marchingCubes->Update();
-    vtkSmartPointer<vtkPolyData> polyData = marchingCubes->GetOutput();
+    vtkSmartPointer<vtkPolyData> result_poly_data = marchingCubes->GetOutput();
 
     vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputData(polyData);
+    mapper->SetInputData(result_poly_data);
 
     vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
     actor->SetMapper(mapper);
 
     vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
     renderer->AddActor(actor);
-    renderer->SetBackground(0.1, 0.2, 0.4); // Set background color to dark blue
+    renderer->SetBackground(0.1, 0.2, 0.4);
 
     vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
     renderWindow->AddRenderer(renderer);
